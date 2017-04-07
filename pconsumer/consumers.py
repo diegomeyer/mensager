c9b24732609390328cf7d43f072fa6e0ff2d548b
diego.meyer <diego.meyer@luz-ef.com>
2017-04-07 14:44:14 -0300

    login

--- pconsumer/consumers.py
+++ pconsumer/consumers.py
@@ -1,15 +1,12 @@
+import json
 from django.http import HttpResponse
 from channels import Group
 from channels.handler import AsgiHandler
 from channels.sessions import channel_session
+from pconsumer.models import Room
 from channels.auth import channel_session_user, channel_session_user_from_http
-
-def http_consumer(message):
-    # Make standard HTTP response - access ASGI path attribute directly
-    response = HttpResponse("Hello world! You asked for %s" % message.content['path'])
-    # Encode that response into message format (ASGI)
-    for chunk in AsgiHandler.encode_response(response):
-        message.reply_channel.send(chunk)
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 @channel_session_user_from_http
 def ws_add(message):
@@ -17,16 +14,17 @@ def ws_add(message):
     # Accept connection
     message.reply_channel.send({"accept": True})
     # Add them to the right group
+    message.channel_session['chats'] = []
     Group("chat", channel_layer=message.channel_layer).add(message.reply_channel)
-
-
+    message.channel_session['chats'] = list(set(message.channel_session['chat']).union([1]))
+ 
+ 
 @channel_session_user
 def ws_message(message):
-    print('menssagem')
     Group("chat", channel_layer=message.channel_layer).send({
         "text": message['text'],
     })
-
+ 
 @channel_session_user_from_http
 def ws_add_id(message):
     print('group1')
@@ -34,24 +32,24 @@ def ws_add_id(message):
     message.reply_channel.send({"accept": True})
     # Add them to the right group
     Group("chat1", channel_layer=message.channel_layer).add(message.reply_channel)
-
-@channel_session_user
+ 
+@channel_session_user 
 def ws_message_id(message):
     print('menssagem1')
     Group("chat1", channel_layer=message.channel_layer).send({
         "text": message['text'],
     })
-
+ 
 # Connected to websocket.disconnect
-@channel_session_user
+@channel_session_user 
 def ws_disconnect(message):
-    Group("chat-%s" % message.user.username[0]).discard(message.reply_channel)
-
+    Group("chat").discard(message.reply_channel)
+ 
 @channel_session_user
 def ws_disconnect_id(message):
-    Group("chat1-%s" % message.user.username[0]).discard(message.reply_channel)
-    
-    
+    Group("chat1").discard(message.reply_channel)
+     
+     
 @channel_session
 def ws_connect(message):
     # Accept connection
